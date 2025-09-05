1a155ba90184cce1efdb532fa7688795ae0b8edd
Michael Gaitho <michaelgaitho47@gmail.com>
2025-09-05 15:16:51 +0300

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav>
+    <ul class="flex space-x-4">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+</template>
