2ddef031ecb2a1f9eecf4d90fd898bb2fef2d0b7
erico <njukieric233@gmail.com>
2025-09-12 16:55:30 +0300

    updates

--- src/pages/Home/Hero.vue
+++ src/pages/Home/Hero.vue
@@ -1,11 +1,15 @@
 <template>
-    <!-- Hero section -->
-        <section class="relative w-full h-screen flex items-center justify-center overflow-hidden"> 
-        <img 
-            :src="churchImg"
-            class="absolute inset-0 w-full h-full object-cover"
-            alt="Church Background"
-        />
+  <section class="relative w-full h-screen flex items-center justify-center overflow-hidden">
+    <video
+      autoplay
+      muted
+      loop
+      playsinline
+      class="absolute inset-0 w-full h-full object-cover"
+      :src="churchVideo"
+    />
+    <div class="absolute inset-0 bg-black/40"></div>
+    <div class="relative z-10 flex flex-col items-center justify-center w-full h-full">
         <div class="relative z-10 flex flex-col items-center justify-center w-full h-full bg-black/40">
             <hgroup data-aos="fade-up" data-aos-delay="500">
                 <h1 class="font-serif text-4xl text-white md:text-6xl lg:text-7xl text-center font-bold mb-6 animate-fade-up">
@@ -16,9 +20,10 @@
                 </p>
             </hgroup>
         </div>
-    </section>
+    </div>
+  </section>
 </template>
 
 <script setup>
-import churchImg from '../../assets/images/Church.png'
+import churchVideo from '../../assets/videos/church.mp4'
 </script>
