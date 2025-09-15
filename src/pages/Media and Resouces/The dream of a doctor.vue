cfaf9144e34141ff9a2f4fb49c50f9ee979b6457
erico <njukieric233@gmail.com>
2025-09-15 13:33:22 +0300

    updates

---
+++ src/pages/Media and Resouces/The dream of a doctor.vue
@@ -0,0 +1,109 @@
+<template>
+  <section class="max-w-6xl mx-auto px-6 py-12 pt-30 space-y-16">
+    <!-- Top Row -->
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <!-- Text -->
+      <div>
+        <h2 class="text-2xl font-bold text-gray-800 mb-4">
+          The Dream of a Doctor
+        </h2>
+        <p class="text-gray-700 leading-relaxed mb-6">
+          In the small town of Naishi-Lare, nestled in the heart of Njoro Sub-
+          County, resides an extraordinary individual who has overcome numerous
+          obstacles to achieve her dreams. Meet Janet Wanjiku, a young woman
+          whose unyielding determination and passion for education have shone
+          through even in the face of adversity.
+        </p>
+        <p class="text-gray-700 leading-relaxed mb-6">
+          Born into a family with limited resources, Janet’s father, a
+          hardworking man, struggled to provide for his children’s education
+          after losing his job at the flower farm due to the COVID pandemic.
+          With two younger siblings in high school and his eldest daughter,
+          Janet, offered a spot at KMTC for nursing, the financial burden seemed
+          insurmountable. Yet, through the support of relatives and her father’s
+          resilience, Janet managed to enrol in high school.
+        </p>
+        <img
+          src="/images/24.jpeg"
+          alt="FinTech Hub Launch"
+          class="rounded-lg shadow-md mb-6"
+        />
+      </div>
+      <!-- Image -->
+      <div>
+        <img
+          src="/images/61-1.jpeg"
+          alt="FinTech Hub Launch"
+          class="rounded-lg shadow-md mb-6"
+        />
+        <p class="text-gray-700 leading-relaxed mb-6">
+          Throughout her high school journey, Janet encountered various
+          challenges, from purchasing uniforms to finding a way to pay her
+          school fees. She was lucky to get a sponsor “Baba Njeri” who paid her
+          school fees until she sat for her final exams. The support she allowed
+          Janet to excel academically and score a remarkable B+ in her form 4
+          exams.
+        </p>
+        <p class="text-gray-700 leading-relaxed mb-6">
+          Unfortunately, despite her achievements, the lack of financial means
+          meant she couldn’t fulfill her dream of joining Masinde Muliro
+          University. Undeterred, Janet continued to seek opportunities,
+          applying to KMTC multiple times without success. This didn’t dampen
+          her spirits; instead, she focused on being a role model for her
+          younger sister, Lucy Wanju, and all her siblings, encouraging them to
+          study hard and listen to their parents’ guidance.
+        </p>
+      </div>
+    </div>
+
+    <!-- Bottom Row -->
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <!-- Image -->
+      <div>
+        <p class="text-gray-700 leading-relaxed mb-6">
+          As she was waiting for support to join university, she was called back
+          to Mugumo High School where she taught biology and chemistry with
+          passion. Despite commuting 15 kilometers each day, the little she’s
+          gets appreciated with by the school management, she diligently saves
+          the money, which she plans to use for her admission to university.
+        </p>
+        <p class="text-gray-700 leading-relaxed mb-6">
+          During home visit to Janet’s place, she narrated to us of her career
+          goals and how much she’d want to be a doctor. “If I can’t become a
+          doctor,” Janet said, “I’m going to spoonfeed my future child to be a
+          doctor.” This were such powerful statements that shows her drive to
+          succeed in life.
+        </p>
+        <img
+          src="/images/34.jpeg"
+          alt="Event Audience"
+          class="rounded-lg shadow-md mb-6"
+        />
+        <p class="text-gray-700 leading-relaxed mb-6">
+            Janet Wanjiku is an inspiration to us all. She shows us that anything is possible if we have the determination and resilience to never give up on our dreams. Currently, R.O.C.K Bridge ministries has facilitated her admission at Kenyatta University to pursue a degree in community health nursing.
+        </p>
+      </div>
+      <!-- Text -->
+      <div>
+        <img
+          src="/images/38.jpeg"
+          alt="Event Audience"
+          class="rounded-lg shadow-md mb-6"
+        />
+        <p class="text-gray-700 leading-relaxed mb-6">
+          Janet’s motivation is deeply rooted in her desire to uplift her family
+          and break the cycle of poverty. Her resilience, discipline, and hard
+          work have earned her respect not only from her parents but also from
+          her neighbours and community.
+        </p>
+        <img
+          src="/images/36-1.jpeg"
+          alt="Guests at FinTech Hub"
+          class="rounded-lg shadow-md mb-6"
+        />
+      </div>
+    </div>
+  </section>
+  
+  
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -15,8 +15,25 @@ import HowToSupport from '../pages/Get involved/HowToSupport.vue'
 import PartnerWithUs from '../pages/Get involved/PartnerWithUs.vue'
 import FundOurPrograms from '../pages/Get involved/Fundourprograms.vue'
 import MediaAndResources from '../pages/Media and Resouces/Media and resources.vue'
+import GrandLaunch from '../pages/Media and Resouces/Grand Launch.vue'
+import CETCT from '../pages/Media and Resouces/CETCT.vue'
+import MDDSPG from '../pages/Media and Resouces/MDDSPG.vue'
 import Contact from '../pages/Contact.vue'
 import Home from '../pages/Home.vue'
+import JourneyToNursing from '../pages/Media and Resouces/journey to nursing.vue'
+import TheDreamOfADoctor from '../pages/Media and Resouces/The dream of a doctor.vue'
+import HydrologyDegree from '../pages/Media and Resouces/Hydrology Degree.vue'
+import RBScholarship from '../pages/Media and Resouces/RB scholarship.vue'
+import HolidayHomesPhotos from '../pages/Media and Resouces/Holiday homes photos.vue'
+import CleanWaterInitiatives from '../pages/Media and Resouces/Clean water intitiative.vue'
+import LishAiLabs from '../pages/Media and Resouces/Lish ai Labs.vue'
+import MedicalClinicPhotos from '../pages/Media and Resouces/Medical clinic photos.vue'
+import MedicalTripVideo from '../pages/Media and Resouces/videos/Medical trip video.vue'
+import NursingCollegeVideo from '../pages/Media and Resouces/videos/Nursing college video.vue'
+import OverviewVideo from '../pages/Media and Resouces/videos/Overview.vue'
+import TumainiCenterVideo from '../pages/Media and Resouces/videos/Tumaini center video.vue'
+import WorkplaceVideo from '../pages/Media and Resouces/videos/Work place video.vue'
+import YouthVideo from '../pages/Media and Resouces/videos/Youth video.vue'
 
 const routes = [
 
@@ -84,6 +101,91 @@ const routes = [
     path: '/media-and-resources',
     name: 'MediaAndResources',
     component: MediaAndResources
+  },
+  {
+    path: '/media-and-resources/grand-launch',
+    name: 'GrandLaunch',
+    component: GrandLaunch
+  },
+  {
+    path: '/media-and-resources/CETCT',
+    name: 'CETCT',
+    component: CETCT
+  },
+  {
+    path: '/media-and-resources/MDDSPG',
+    name: 'MDDSPG',
+    component: MDDSPG
+  },
+  {
+    path: '/media-and-resources/journey-to-nursing',
+    name: 'journey-to-nursing',
+    component: JourneyToNursing
+  },
+  {
+    path: '/media-and-resources/the-dream-of-a-doctor',
+    name: 'the-dream-of-a-doctor',
+    component: TheDreamOfADoctor
+  },
+  {
+    path: '/media-and-resources/hydrology-degree',
+    name: 'hydrology-degree',
+    component: HydrologyDegree
+  },
+  {
+    path: '/media-and-resources/rb-scholarship',
+    name: 'rb-scholarship',
+    component: RBScholarship
+  },
+  {
+    path: '/media-and-resources/holiday-homes-photos',
+    name: 'holiday-homes-photos',
+    component: HolidayHomesPhotos
+  },
+  {
+    path: '/media-and-resources/clean-water-initiatives',
+    name: 'clean-water-initiatives',
+    component: CleanWaterInitiatives
+  },
+  {
+    path: '/media-and-resources/lish-ai-labs',
+    name: 'lish-ai-labs',
+    component: LishAiLabs
+  },
+  {
+    path: '/media-and-resources/medical-clinic-photos',
+    name: 'medical-clinic-photos',
+    component: MedicalClinicPhotos
+  },
+  {
+    path: '/media-and-resources/medical-trip-video',
+    name: 'medical-trip-video',
+    component: MedicalTripVideo
+  },
+  {
+    path: '/media-and-resources/nursing-college-video',
+    name: 'nursing-college-video',
+    component: NursingCollegeVideo
+  },
+  {
+    path: '/media-and-resources/overview-video',
+    name: 'overview-video',
+    component: OverviewVideo
+  },
+  {
+    path: '/media-and-resources/tumaini-center-video',
+    name: 'tumaini-center-video',
+    component: TumainiCenterVideo
+  },
+  {
+    path: '/media-and-resources/workplace-video',
+    name: 'workplace-video',
+    component: WorkplaceVideo
+  },
+  {
+    path: '/media-and-resources/youth-video',
+    name: 'youth-video',
+    component: YouthVideo
   },
   {
     path: '/contact',
